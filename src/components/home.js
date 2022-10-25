74351ab01ebdbf312a1cbf3a24a4f03b39dc1217
Harish <harishpranesh983@gmail.com>
2022-10-25 10:53:56 +0530

    s1

---
+++ src/components/home.js
@@ -0,0 +1,67 @@
+import "./home.css";
+import {useEffect, useState} from "react";
+
+function Home(props)
+{
+    const [post,setp]=useState([])
+    const d = new Date()
+    let x=0
+    let temp
+
+    function scr(){document.getElementById("chats").scrollTop=document.getElementById("chats").scrollHeight;}
+
+    function fet(){
+        fetch("https://tranquil-wave-46545.herokuapp.com/").then(
+                (res) => res.json()
+            ).then((j)=>{
+                temp=j
+                if(temp.length>x)
+                {
+                    x=temp.length
+                    setp(temp)
+                }
+            })}
+            
+    useEffect(()=>{
+    return(()=>setInterval(()=>{fet()
+    },1000))
+    },[])
+
+    useEffect(()=>{
+    scr()
+    },[post])
+    
+    const sub=(e)=>{
+        if(document.getElementById("typing").value.trim() == "")
+        return
+
+        fetch("https://tranquil-wave-46545.herokuapp.com/",{
+            method : "post",
+            headers : {"Content-Type":"application/json"},
+            body: JSON.stringify({id:props.Id,msg:document.getElementById("typing").value.trim(),date:d.getHours()+":"+((d.getMinutes()<10)?"0":"")+d.getMinutes()+"\t"+d.getDate()+"/"+d.getMonth()})}).then(e=>console.log("posted"))
+            document.getElementById("typing").value="";
+    }
+    
+    return(
+        <div className="home">
+            <div className="nav"><p className="kuruk">Kuruk</p></div>
+            <div className="chats" id="chats">
+                {
+                    post.map((e)=>
+                        <div className="chat">
+                            <p>{e.id}</p>
+                            <p>{e.msg}</p>
+                            <p className="date">{e.date}</p>
+                        </div>
+                    )
+                }
+            </div>
+            <div className="chatbox">
+            <input className="typing" id="typing" placeholder="type ur text" onFocus={(e)=>{e.target.placeholder=" "}} autoComplete="off" onBlur={(e)=>{e.target.placeholder="type ur text"}}/>
+            <button onClick={sub} className="send">send</button>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/components/login.js
@@ -0,0 +1,106 @@
+import "./login.css";
+import Home from "./home";
+import {useState} from "react";
+
+function Login() {
+    
+    const post=["h","Harish","Thangam","Harini","Dhanush","Sush","Techpak","Javid","Crimson","Kuttichathan","Dash","Sankar"]
+    const passes=["harish#1","thangam&27","harini@7","dhanush$9","sushanthi^1","deepak%1","akash*16","reshma+14","deeyes!3","sankar=22"]
+    const [d,setd]=useState(0);
+    const [userid,setud]=useState("");
+
+    const newaccount = (e) => {
+        if (document.getElementsByClassName("newacc")[0].innerHTML == 'Login Here') {
+            document.getElementsByClassName("retype")[0].style.display = "none";
+            document.getElementsByClassName("newacc")[0].innerHTML = 'Sign up';
+            document.getElementsByClassName("newacc")[0].style.marginTop = "4vh";
+            document.getElementsByClassName("head")[0].innerHTML = "Login";
+        }
+        else {
+            document.getElementsByClassName("retype")[0].style.display = "inline";
+            document.getElementsByClassName("newacc")[0].innerHTML = 'Login Here';
+            document.getElementsByClassName("newacc")[0].style.marginTop = "4vh";
+            document.getElementsByClassName("submit")[0].style.marginTop = "2.5vh";
+            document.getElementsByClassName("head")[0].innerHTML = "Create account";
+        }
+    }
+
+    const submit = (e) => {
+        if (document.getElementById("user").value.trim() == "") {
+            document.getElementsByClassName("userincorrect")[0].style.display = "inline";
+            document.getElementsByClassName("password")[0].style.marginTop = "1vh";
+            document.getElementsByClassName("passwordincorrect")[0].style.display = "none";
+            document.getElementsByClassName("retypeincorrect")[0].style.display = "none";
+            return;
+        }
+        if (document.getElementById("pass").value.trim() == "" || document.getElementById("pass").value.trim().length<8) {
+            document.getElementsByClassName("passwordincorrect")[0].style.display = "inline";
+            document.getElementsByClassName("retype")[0].style.marginTop = "1vh";
+            document.getElementsByClassName("newacc")[0].style.marginTop = "2vh";
+            document.getElementsByClassName("retypeincorrect")[0].style.display = "none";
+            return;
+        }
+        if (document.getElementsByClassName("newacc")[0].innerHTML == 'Login Here') {
+            if (document.getElementById("retype").value.trim() == "" || document.getElementById("retype").value.trim()!=document.getElementById("pass").value.trim()) 
+            {
+                document.getElementsByClassName("retypeincorrect")[0].style.display = "inline";
+                document.getElementsByClassName("newacc")[0].style.marginTop = "0vh";
+                return;
+            }
+            if(post.includes(document.getElementById("user").value.trim()))
+            {
+                alert("Email already used");
+            }
+        }
+        else
+        {
+            if(post.includes(document.getElementById("user").value.trim()) && passes.includes(document.getElementById("pass").value.trim()))
+            {
+                document.getElementsByClassName("login")[0].style.display="none";
+                setd(1);
+                setud(document.getElementById("user").value.trim());
+
+            }
+            else
+            {
+                document.getElementsByClassName("passwordincorrect")[0].style.display = "inline";
+            }
+        }
+        
+    }
+
+    return (
+        <div className="background">
+            {d==1?<Home Id={userid}/>:
+            <div className="login">
+                <h1 className="head">Login</h1>
+                <input className="user" id="user" autoComplete="off" spellCheck="false"
+                    onFocus={(e) => {
+                        e.target.placeholder = ''; document.getElementsByClassName("userincorrect")[0].style.display = "none";
+                        document.getElementsByClassName("password")[0].style.marginTop = "4vh";
+                    }}
+                    onBlur={(e) => { e.target.placeholder = 'user' }}
+                    placeholder='user' />
+                <p className="userincorrect" >Please fill valid info</p>
+                <input className="password" id="pass" autoComplete="off" spellCheck="false"
+                    onFocus={(e) => {
+                        e.target.placeholder = ''; document.getElementsByClassName("passwordincorrect")[0].style.display = "none";
+                        document.getElementsByClassName("retype")[0].style.marginTop = "4vh";
+                    }}
+                    onBlur={(e) => { e.target.placeholder = 'password' }} placeholder='password' />
+                <p className="passwordincorrect" >Please fill valid info</p>
+                <input className="retype" id="retype" autoComplete="off" spellCheck="false"
+                    onFocus={(e) => {
+                        e.target.placeholder = ''; document.getElementsByClassName("retypeincorrect")[0].style.display = "none";
+                        document.getElementsByClassName("newacc")[0].style.marginTop = "4vh";
+                    }}
+                    onBlur={(e) => { e.target.placeholder = 'retype' }} placeholder='retype' />
+                <p className="retypeincorrect" >Please fill valid info</p>
+                <span className="newacc" >Sign up</span>
+                <button className="submit" onClick={submit}>Submit</button>
+                </div>}
+            </div>
+    );
+}
+
+export default Login;
